3994b29837311f6eb7c7251454c4c45d11c042e3
Soomin <o3o_121@naver.com>
2025-01-06 11:34:25 +0900

    Feat: 자바 2일차
    Body: 제어문(If, Switch)

---
+++ src/main/java/main/org/example/IfTest.java
@@ -0,0 +1,34 @@
+package main.org.example;
+
+public class IfTest {
+
+    public static void main(String[] args) {
+
+        // 랜덤 int 값
+        int value = (int)(Math.random() * 6) + 1;
+
+        System.out.println(value);
+
+        int oddEven = value % 2;    // 2로 나눈 나머지 0, 1
+
+        // 만일 oddEven 값이 0이면 짝수 출력 ==> boolean
+        if (oddEven == 0) {
+            System.out.println("짝수");
+        } else {    // 만약 oddEven 값이 1이면 홀수 출력
+            System.out.println("홀수");
+        }
+
+
+        // 또 다른 방법 (강사님이 선호함)
+        if (oddEven == 0) {
+            System.out.println("짝수");
+            return;
+        }
+            System.out.println("홀수");
+
+
+        // 삼항연산자 조건식(결과) ? 참인 경우 결과 : 거짓인 경우
+        String result = oddEven == 1 ? "홀수" : "짝수";
+
+    }
+}
---
+++ src/main/java/main/org/example/RPSGameEx.java
@@ -0,0 +1,30 @@
+package main.org.example;
+
+public class RPSGameEx {
+
+    public static void main(String[] args) {
+        // 사용자 값
+        int user = 2;
+        // 컴 값
+        int com = 0;
+
+        // user 값이 com 보다 작을 경우
+        user = user < com ? user + 3 : user;
+
+        int result = user - com;
+
+        switch (result) {
+            case 0:
+                System.out.println("비김");
+                break;
+            case 1:
+                System.out.println("이김");
+                break;
+            case 2:
+                System.out.println("짐");
+                break;
+        }
+
+
+    }
+}
