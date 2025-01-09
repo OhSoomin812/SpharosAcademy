b8b366d24ec20ad17e215f5414cb7b70dec5b66e
Soomin <o3o_121@naver.com>
2025-01-09 23:42:14 +0900

    Feat: 자바 5일차 (클래스)

--- src/main/java/org/example/snakegame/Tile.java
+++ src/main/java/org/example/snakegame/Tile.java
@@ -7,8 +7,17 @@ import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
+@Setter
 @ToString
 public class Tile {
 
     private int num;
+    private String desc;
+    private String type;
+    private int moving;
+
+    public Tile(int num) {
+        this.num = num;
+    }
 }
+
