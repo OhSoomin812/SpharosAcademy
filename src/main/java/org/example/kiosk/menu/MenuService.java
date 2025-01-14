29b97af7a36243638d7da04e54e5e0a692ae68ed
Soomin <o3o_121@naver.com>
2025-01-14 16:38:00 +0900

    Fix: 오류 ㅅ줭

--- src/main/java/org/example/kiosk/menu/MenuService.java
+++ src/main/java/org/example/kiosk/menu/MenuService.java
@@ -16,11 +16,12 @@ public enum MenuService {
         this.menus = new ArrayList<>();
         menus.add(new Menu(1, "아메리카노", 3000));
         menus.add(new Menu(2, "라떼", 3500));
-        menus.add(new Menu(1, "카푸치노", 4000));
-        menus.add(new Menu(1, "에스프레소", 2500));
+        menus.add(new Menu(3, "카푸치노", 4000));
+        menus.add(new Menu(4, "에스프레소", 2500));
     }
 
     public ArrayList<Menu> getMenus() {
         return menus;
     }
+
 }
